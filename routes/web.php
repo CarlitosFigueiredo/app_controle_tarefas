b96cf03b161768d9668715be37e631edf9424cdb
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-02-06 10:22:23 -0300

    Aplicando verfied middleware e customizando templates

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home')
+    ->middleware('verified');
 
-Route::resource('/tarefa', TarefaController::class);
+Route::resource('/tarefa', TarefaController::class)
+    ->middleware('verified');
 
-Route::get('/mensagem-teste', function() {
+Route::get('/mensagem-teste', function () {
 
     // Ver o modelo do e-mail
     return new MensagemTesteMail();
@@ -22,5 +25,4 @@ Route::get('/mensagem-teste', function() {
     // Enviar um e-mail para teste...
     // Mail::to('medeirosfigueiredoc@gmail.com')->send(new MensagemTesteMail());
     // return 'E-mail enviado com sucesso!';
-
 });
